8d04ded9a1a0c7b4d64e1699d856e6fb7498389e
opilane <opilane@HP-CZC2349HTP>
2025-11-27 08:53:05 +0200

    käärid

--- paber, kärid ja kivi.py
+++ paber, kärid ja kivi.py
@@ -1,14 +1,14 @@
 #paber, käärid ja kivi mäng 
 import random
 
-valikud = ["kivi", "paber", "käärid"]
-mangijad = ["Inimene", "Robot"]
+valikud=["kivi", "paber", "käärid"]
+mangijad=["Inimene", "Robot"]
 random.shuffle(mangijad)
-print("Mängijate järjekord:", mangijad)
-punktid = {"Inimene": 0, "Robot": 0}
+print(f"Mängijate järjekord: {mangijad}")
+punktid={"Inimene": 0, "Robot": 0}
+vooru_valikud=[]
+vooru_tulemused=[]
 
-vooru_valikud = []
-vooru_tulemused = []
 while True:
     while True:
         kasutaja_valik = input("Sisesta kivi, paber või käärid: ").lower()
@@ -16,33 +16,36 @@ while True:
             break
         else:
             print("Vale valik! Palun proovi uuesti.")
-    arvuti_valik = random.choice(valikud)
+
+    arvuti_valik=random.choice(valikud)
     print(f"Robot valis: {arvuti_valik}")
 
     vooru_valikud.append((kasutaja_valik, arvuti_valik))
-    if kasutaja_valik == arvuti_valik:
-        tulemus = "Viik"
+
+    if kasutaja_valik==arvuti_valik:
+        tulemus="Viik"
         print("Viik!")
-    elif (kasutaja_valik == "kivi" and arvuti_valik == "käärid") or \
-         (kasutaja_valik == "paber" and arvuti_valik == "kivi") or \
-         (kasutaja_valik == "käärid" and arvuti_valik == "paber"):
-        tulemus = "Inimene võitis"
-        punktid["Inimene"] += 1
+    elif (kasutaja_valik=="kivi" and arvuti_valik == "käärid") or \
+         (kasutaja_valik=="paber" and arvuti_valik == "kivi") or \
+         (kasutaja_valik=="käärid" and arvuti_valik == "paber"):
+        tulemus="Inimene võitis"
+        punktid["Inimene"]+=1
         print("Sa võitsid!")
     else:
-        tulemus = "Robot võitis"
-        punktid["Robot"] += 1
+        tulemus="Robot võitis"
+        punktid["Robot"]+=1
         print("Robot võitis!")
 
     vooru_tulemused.append(tulemus)
-    print("\nPunktid:")
-    print("Inimene:", punktid["Inimene"])
-    print("Robot:", punktid["Robot"])
-    uuesti = input("\nKas mängida järgmine voor? (jah/ei): ").lower()
-    if uuesti != "jah":
+
+    print("Punktid:")
+    print(f"Inimene: {punktid['Inimene']}")
+    print(f"Robot: {punktid['Robot']}")
+
+    uuesti=input("Kas mängida järgmine voor? (jah/ei): ").lower()
+    if uuesti!="jah":
         break
+
 print("Mäng lõppes")
-print(f"Voorude valikud:{vooru_valikud}")
-print(f"Voorude tulemused:{vooru_tulemused}")
-print(f"Lõplik tabel:{punktid}")
+print(f"Lõplik tabel: {punktid}")
 
