ac98c9fa0582bc6a4dd7fa59bcab85df303c04bf
opilane <opilane@HP-CZC2349HV1>
2025-12-04 11:41:43 +0200

    yayy

--- Moodulifail.py
+++ Moodulifail.py
@@ -1,1 +1,107 @@
+#töö 5.1
+#1
+from binascii import a2b_qp
 
+
+def float_kontroll(sisend:str)->float:
+    """Kontrollib, kas sisestatud andmed on teisendatavad float arvuks
+    :param str sisend: kasutaja sisestatud andmed
+    :return/rtype: teisendatud float arv
+    """
+    while True:
+        try:
+            arv=float(sisend)
+            return arv
+        except ValueError:
+            sisend=input("Palun sisesta ainult arv: ")
+
+#2
+
+def int_kontroll(sisend:str)->int:
+    """Kontrollib, kas sisestatud andmed on teisendatavad int arvuks
+    :param str sisend: kasutaja sisestatud andmed
+    :return/rtype: teisendatud float arv
+    """
+    while True:
+        try:
+            arv=int(sisend)
+            return arv
+        except ValueError:
+            sisend=input("Palun sisesta arv: ")
+
+#1
+
+def arithmetic(a:float, b:float, tehe:str)->any:
+    """Lihtne kalkulaator:
+    + - liitmine
+    - - lahutamine
+    * - korrutamine
+    / - jagamine
+    Muul juhul tagastab "Tundmatu tehe"
+    :param float a: esimene arv
+    :param float b: teine arv
+    :param str tehe: tehe, mis tuleb teha
+    :return/rtype: tehte tulemus float või str
+    """
+    if tehe in ["+", "-", "*", "/"]:
+        if tehe=="/" and b==0:
+            vastus= "Nulliga jagamine pole lubatud."
+        else:
+            vastus=eval(f"{a}{tehe}{b}")
+    else:
+        vastus="Tundmatu tehe"
+    return vastus
+
+#2
+def is_year_leap(aasta:int)->bool:
+    """Kontrollib, kas aasta on liigaasta
+    :param int aasta: aasta arvuna
+    :return/rtype: True kui liigaasta, False kui tavaline aasta
+    """
+    if (aasta % 4 == 0 and aasta % 100 != 0):
+        return True
+    else:
+        return False
+
+#3
+def square(külg:float)->any:
+    """Arvutab ruudu ümbermõõdu, pindala ja diagonaali
+    :param float külg: ruudu külje pikkus
+    :return/rtype: ümbermõõt, pindala, diagonaal
+    """
+    ümbermõõt=4*külg
+    pindala=külg**2 #või külg*külg
+    diagonaal=külg**0.5*2
+    return (ümbermõõt, pindala, diagonaal)
+
+
+#4
+
+def season(kuu:int)->str:
+    """tagastab aastaaja kuu numbri põhjal
+    :param int kuu: kuu arvuna (1-12)
+    :return/rtype: hooaja aastaja nimetus str
+    """
+    if kuu in [12, 1, 2]:
+        return "talv"
+    elif kuu in [3, 4, 5]:
+        return "kevad"
+    elif kuu in [6, 7, 8]:
+        return "suvi"
+    elif kuu in [9, 10, 11]:
+        return "sügis"
+    else:
+        return "vigane kuu number"
+
+#5
+
+def bank(a: float, years:int)->float:
+    """Arvutab lõppsumma koos intressiga
+    :param float a: algsumma
+    :param int years: aastate arv
+    :return/rtype: lõppsumma float
+    """
+    intress=0.1 #10% intress
+    for i in range(years):
+        a+=a*intress
+    return a
--- Tavalinefail.py
+++ Tavalinefail.py
@@ -1,1 +1,45 @@
+from Moodulifail import *
+
+#5
+summa=float(input("Sisesta hoiuse summa: "))
+aastad=int(input("Sisesta aastate arv: "))
+lõpp_summa=bank(summa, aastad)
+print(f"Lõppsumma {aastad} aasta pärast on {lõpp_summa:.2f} eurot.")
+
+#4
+kuu=season(input("Sisesta kuu number (1-12): "))
+print(f"Aastaaja on: {kuu}")
+
+
+#3
+print("Arvutame ruudu pindala ja ümbermõõtu!")
+külg=float_kontroll(input("Sisesta ruudu külje pikkus: "))
+ümbermõõt,pindala,diagonaal=square(külg)
+print(f"Ruudu külg: {külg}")
+print(f"Ruudu ümbermõõt: {ümbermõõt}")
+print(f"Ruudu pindala: {pindala}")
+print(f"Ruudu diagonaal: {diagonaal}")
+
+
+
+#2
+for i in range(3):
+    aasta=int_kontroll(input("Sisesta aasta: "))
+    if is_year_leap(aasta):
+        print(f"{aasta} on liigaasta")
+    else:
+        print(f"{aasta} on tavaline aasta")
+
+
+
+  #1
+print("Lahendame/testimine mitu arvutehet!")
+for i in range(5):
+
+    arv1=float_kontroll(input("Sisesta esimene arv: "))
+    arv2=float_kontroll(input("Sisesta teine arv: "))
+
+    t=input("Sisesta tehe (+,-,*,/): ")
+    tulemus=arithmetic(arv1,arv2,t)
+    print(f"{arv1}{t}{arv2} = {tulemus}")
 
